853572a3ebaa9f3ba12bb62bd76c1b964658a83b
LitchiCheng <974782852@qq.com>
2024-04-18 22:19:19 +0800

    feat:add annotation

--- testMNIST.py
+++ testMNIST.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class CNN(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self):
         super(CNN, self).__init__()
-
+        # 连接序列
         self._conv1_layer = nn.Sequential(
+            # 卷积
             nn.Conv2d(1,15,5),
+            # 激活函数
             nn.ReLU(),
+            # 最大池化，减少特征量，选特征最大的数，是一种下采样
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
         
@@ -21,20 +24,23 @@ class CNN(nn.Module):
         )
 
         self._full_layer = nn.Sequential(
+            # 卷积层都是四维张量，展平为二维张量给连接层用
             nn.Flatten(),
             nn.Linear(in_features=480, out_features=60),
             nn.ReLU(),
             nn.Linear(in_features=60, out_features=10),
         )
-
+    
     def forward(self, input):
+        # 层层连接，两个卷积层，最后全连接层
         output = self._conv1_layer(input)
         output = self._conv2_layer(output)
         output = self._full_layer(output)
         return output
 
 class Test:
-    def __init__(self) -> None:
+    def __init__(self):
+        # MNIST数据集，用于训练，一次抓60 size
         self._train_loader = torch.utils.data.DataLoader(
             torchvision.datasets.MNIST('./data/', train=True, download=True,
                                     transform=torchvision.transforms.Compose([
@@ -43,6 +49,7 @@ class Test:
                                             (0.1307,), (0.3081,))
                                     ])),
             batch_size=60, shuffle=True)
+        # 用于测试，一次抓500 size
         self._test_loader = torch.utils.data.DataLoader(
             torchvision.datasets.MNIST('./data/', train=False, download=True,
                                     transform=torchvision.transforms.Compose([
@@ -51,10 +58,12 @@ class Test:
                                             (0.1307,), (0.3081,))
                                     ])),
             batch_size=500, shuffle=True)
-
+        # 训练次数
         self._epochs = 3
         self._cnn = CNN()
+        # 交叉熵损失函数，刻画的是两个概率分布的距离，交叉熵越小，概率分布越接近
         self._loss_func = nn.CrossEntropyLoss()
+        # 优化器
         self._optim = torch.optim.Adam(self._cnn.parameters(), lr=0.01)
         if torch.cuda.is_available():
             print("Use CUDA training!")
@@ -68,13 +77,16 @@ class Test:
         for epoch in range(1, self._epochs + 1):
             self._cnn.train(mode=True)
             for idx, (train_img, train_label) in enumerate(self._train_loader):
+                # 复制到device中
                 train_img = train_img.to(self._device)
                 train_label = train_label.to(self._device)
-
                 outputs = self._cnn(train_img)
+                # 清除梯度
                 self._optim.zero_grad()
                 loss = self._loss_func(outputs, train_label)
+                # 反向传播  
                 loss.backward()
+                # 更新权重
                 self._optim.step()
                 # print('Train epoch {}: loss: {:.6f}'.format(epoch,loss.item()))
                 loss_d.append(loss.item())
